0ad5ff89c5a50a4ffda4666c562695304eb51080
nubs29 <edzusvitols@gmail.com>
2023-11-16 14:52:48 +0200

    Update spele.py
    2. stunda

--- spele.py
+++ spele.py
@@ -12,13 +12,27 @@ def btnClick(button):
         button["text"] = "X"
         speletajsX = False
         count += 1
+        checkWinner()
     elif button["text"] == " " and speletajsX == False:
         button["text"] = "O"
         speletajsX = True
         count += 1
+        checkWinner()
     else:
         messagebox.showerror("TicTacToe", "Šeit kāds ir ieklikšķinājis")
     return
+
+    def checkWinner():
+        global winner
+        winner = False
+    if (btn1["text"] == "X" and btn2["text"] == "X" and btn3["text"] == "X" or btn4["text"] == "X" and btn5["text"] == "X" and btn6["text"] == "X" or btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X" or btn1["text"] == "X" and btn5["text"] == "X" and btn9["text"] == "X" or btn3["text"] == "X" and btn5["text"] == "X" and btn7["text"] == "X"):
+        winner = True
+        messagebox.showinfo("TicTacToe", "SpeletajsX ir uzvarējis!")
+    elif (btn1["text"] == "O" and btn2["text"] == "O" and btn3["text"] == "O" or btn4["text"] == "O" and btn5["text"] == "O" and btn6["text"] == "O" or btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O" or btn1["text"] == "O" and btn5["text"] == "O" and btn9["text"] == "O" or btn3["text"] == "O" and btn5["text"] == "O" and btn7["text"] == "O"):
+        winner = True
+        messagebox.showinfo("TicTacToe", "SpeletajsO ir uzvarējis!")
+    elif count == 9 and winner == False:
+        messagebox.showinfo("TicTacToe", "Neizšķirts")
 
 btn1 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn1))
 btn2 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn2))
