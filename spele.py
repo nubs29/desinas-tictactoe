86e6a301b5a0786c46788e5e38a13f2a316a53fd
nubs29 <edzusvitols@gmail.com>
2023-11-16 14:20:11 +0200

    Update spele.py

--- spele.py
+++ spele.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import messagebox
 mansLogs = Tk()
 mansLogs.title("TicTacToe")
+speletajsX = True
+count = 0
 
 def btnClick(button):
     global speletajsX, count
@@ -10,16 +12,23 @@ def btnClick(button):
         button["text"] = "X"
         speletajsX = False
         count += 1
+    elif button["text"] == " " and speletajsX == False:
+        button["text"] = "O"
+        speletajsX = True
+        count += 1
+    else:
+        messagebox.showerror("TicTacToe", "Šeit kāds ir ieklikšķinājis")
+    return
 
-btn1 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn2 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn3 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn4 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn5 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn6 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn7 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn8 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
-btn9 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn1 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn1))
+btn2 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn2))
+btn3 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn3))
+btn4 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn4))
+btn5 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn5))
+btn6 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn6))
+btn7 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn7))
+btn8 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn8))
+btn9 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn9))
 
 btn1.grid(row = 0, column = 0)
 btn2.grid(row = 0, column = 1)
