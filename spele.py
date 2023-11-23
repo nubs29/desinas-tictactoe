25d03bf105d26543382d13cd3ad87f2d351b7e8d
nubs29 <edzusvitols@gmail.com>
2023-11-23 14:32:07 +0200

    Update spele.py
    3. stunda

--- spele.py
+++ spele.py
@@ -22,18 +22,6 @@ def btnClick(button):
         messagebox.showerror("TicTacToe", "Šeit kāds ir ieklikšķinājis")
     return
 
-    def checkWinner():
-        global winner
-        winner = False
-    if (btn1["text"] == "X" and btn2["text"] == "X" and btn3["text"] == "X" or btn4["text"] == "X" and btn5["text"] == "X" and btn6["text"] == "X" or btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X" or btn1["text"] == "X" and btn5["text"] == "X" and btn9["text"] == "X" or btn3["text"] == "X" and btn5["text"] == "X" and btn7["text"] == "X"):
-        winner = True
-        messagebox.showinfo("TicTacToe", "SpeletajsX ir uzvarējis!")
-    elif (btn1["text"] == "O" and btn2["text"] == "O" and btn3["text"] == "O" or btn4["text"] == "O" and btn5["text"] == "O" and btn6["text"] == "O" or btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O" or btn1["text"] == "O" and btn5["text"] == "O" and btn9["text"] == "O" or btn3["text"] == "O" and btn5["text"] == "O" and btn7["text"] == "O"):
-        winner = True
-        messagebox.showinfo("TicTacToe", "SpeletajsO ir uzvarējis!")
-    elif count == 9 and winner == False:
-        messagebox.showinfo("TicTacToe", "Neizšķirts")
-
 btn1 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn1))
 btn2 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn2))
 btn3 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn3))
@@ -53,6 +41,61 @@ btn6.grid(row = 1, column = 2)
 btn7.grid(row = 2, column = 0)
 btn8.grid(row = 2, column = 1)
 btn9.grid(row = 2, column = 2)
+
+def checkWinner():
+    global winner
+    winner = False
+    if (btn1["text"] == "X" and btn2["text"] == "X" and btn3["text"] == "X" or btn4["text"] == "X" and btn5["text"] == "X" and btn6["text"] == "X" or btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X" or btn1["text"] == "X" and btn5["text"] == "X" and btn9["text"] == "X" or btn3["text"] == "X" and btn5["text"] == "X" and btn7["text"] == "X" or btn1["text"] == "X" and btn4["text"] == "X" and btn7["text"] == "X" or btn2["text"] == "X" and btn5["text"] == "X" and btn8["text"] == "X" or btn3["text"] == "X" and btn6["text"] == "X" and btn9["text"] == "X"):
+        winner = True
+        messagebox.showinfo("TicTacToe", "SpeletajsX ir uzvarējis!")
+        def disableButtons():
+            btn1.config(status = DISABLED)
+            btn2.config(status = DISABLED)
+            btn3.config(status = DISABLED)
+            btn4.config(status = DISABLED)
+            btn5.config(status = DISABLED)
+            btn6.config(status = DISABLED)
+            btn7.config(status = DISABLED)
+            btn8.config(status = DISABLED)
+            btn9.config(status = DISABLED)
+            return 0
+    elif (btn1["text"] == "O" and btn2["text"] == "O" and btn3["text"] == "O" or btn4["text"] == "O" and btn5["text"] == "O" and btn6["text"] == "O" or btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O" or btn1["text"] == "O" and btn5["text"] == "O" and btn9["text"] == "O" or btn3["text"] == "O" and btn5["text"] == "O" and btn7["text"] == "O" or btn1["text"] == "O" and btn4["text"] == "O" and btn7["text"] == "O" or btn2["text"] == "O" and btn5["text"] == "O" and btn8["text"] == "O" or btn3["text"] == "O" and btn6["text"] == "O" and btn9["text"] == "O"):
+        winner = True
+        messagebox.showinfo("TicTacToe", "SpeletajsO ir uzvarējis!")
+        def disableButtons():
+            btn1.config(status = DISABLED)
+            btn2.config(status = DISABLED)
+            btn3.config(status = DISABLED)
+            btn4.config(status = DISABLED)
+            btn5.config(status = DISABLED)
+            btn6.config(status = DISABLED)
+            btn7.config(status = DISABLED)
+            btn8.config(status = DISABLED)
+            btn9.config(status = DISABLED)
+            return 0
+    elif count == 9 and winner == False:
+        messagebox.showinfo("TicTacToe", "Neizšķirts")
+
+
+
+
+
+
+
+
+
+
+
+def reset():
+    btn1.config(status = NORMAL)
+    btn2.config(status = NORMAL)
+    btn3.config(status = NORMAL)
+    btn4.config(status = NORMAL)
+    btn5.config(status = NORMAL)
+    btn6.config(status = NORMAL)
+    btn7.config(status = NORMAL)
+    btn8.config(status = NORMAL)
+    btn9.config(status = NORMAL)
 
 
 
