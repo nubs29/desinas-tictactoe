b7f59acc52adf562b5b48c73974b089136c7be8b
nubs29 <edzusvitols@gmail.com>
2023-12-07 15:13:04 +0200

    Update spele.py
    07.12.2023

--- spele.py
+++ spele.py
@@ -6,6 +6,51 @@ mansLogs.title("TicTacToe")
 speletajsX = True
 count = 0
 
+def infoLogs():
+    jaunsLogs=Toplevel()
+    jaunsLogs.title('Info par programmu')
+    jaunsLogs.geometry("300x300")
+    apraksts=Label(jaunsLogs,text='Spēles noteikumi: Spēlētāju uzdevums ir panākt, ka viņa simbols (o) atrodas vienā līnijā vertikāli, horizontāli vai pa diagonāli, reizē neļaujot otram spēlētājam vienā līnijā izkārtot savu simbolu (x). Tas, kurš pirmais salicis vienā līnijā savu simbolu, ir uzvarētājs. Ja nevienam spēlētājam neizdodas izvietot savus simbolus vienā līnijā, bet visi lodziņi aizņemti, ir neizšķirts rezultāts.')
+    apraksts.grid(row=0,column=0)
+    return 0
+
+def restart():
+    global winner, count, speletajsX
+    winner=False
+    count=0
+    speletajsX=True
+    return 0
+
+galvenaIzvelne=Menu(mansLogs)
+mansLogs.config(menu=galvenaIzvelne)
+opcijas=Menu(galvenaIzvelne,tearoff=False)
+galvenaIzvelne.add_cascade(label="Opcijas",menu=opcijas)
+galvenaIzvelne.add_command(label="Par programmu",command=infoLogs) 
+
+def reset():
+    btn1.config(state = NORMAL)
+    btn2.config(state = NORMAL)
+    btn3.config(state = NORMAL)
+    btn4.config(state = NORMAL)
+    btn5.config(state = NORMAL)
+    btn6.config(state = NORMAL)
+    btn7.config(state = NORMAL)
+    btn8.config(state = NORMAL)
+    btn9.config(state = NORMAL)
+    btn1["text"]=" "
+    btn2["text"]=" "
+    btn3["text"]=" "
+    btn4["text"]=" "
+    btn5["text"]=" "
+    btn6["text"]=" "
+    btn7["text"]=" "
+    btn8["text"]=" "
+    btn9["text"]=" "
+    return 0
+
+opcijas.add_command(label="Jauna spēle",command=reset)
+opcijas.add_command(label="Iziet",command=mansLogs.quit)
+
 def btnClick(button):
     global speletajsX, count
     if button["text"] == " " and speletajsX == True:
@@ -20,7 +65,7 @@ def btnClick(button):
         checkWinner()
     else:
         messagebox.showerror("TicTacToe", "Šeit kāds ir ieklikšķinājis")
-    return
+    return 0
 
 btn1 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn1), bd = 10, bg = "#97BFB4")
 btn2 = Button(mansLogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"), command = lambda: btnClick(btn2), bd = 10, bg = "#97BFB4")
@@ -42,69 +87,39 @@ btn7.grid(row = 2, column = 0)
 btn8.grid(row = 2, column = 1)
 btn9.grid(row = 2, column = 2)
 
+def disableButtons():
+    btn1.config(state = DISABLED)
+    btn2.config(state = DISABLED)
+    btn3.config(state = DISABLED)
+    btn4.config(state = DISABLED)
+    btn5.config(state = DISABLED)
+    btn6.config(state = DISABLED)
+    btn7.config(state = DISABLED)
+    btn8.config(state = DISABLED)
+    btn9.config(state = DISABLED)
+    return 0
+
 def checkWinner():
     global winner
     winner = False
     if (btn1["text"] == "X" and btn2["text"] == "X" and btn3["text"] == "X" or btn4["text"] == "X" and btn5["text"] == "X" and btn6["text"] == "X" or btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X" or btn1["text"] == "X" and btn5["text"] == "X" and btn9["text"] == "X" or btn3["text"] == "X" and btn5["text"] == "X" and btn7["text"] == "X" or btn1["text"] == "X" and btn4["text"] == "X" and btn7["text"] == "X" or btn2["text"] == "X" and btn5["text"] == "X" and btn8["text"] == "X" or btn3["text"] == "X" and btn6["text"] == "X" and btn9["text"] == "X"):
         winner = True
         messagebox.showinfo("TicTacToe", "SpeletajsX ir uzvarējis!")
+        disableButtons()
     elif (btn1["text"] == "O" and btn2["text"] == "O" and btn3["text"] == "O" or btn4["text"] == "O" and btn5["text"] == "O" and btn6["text"] == "O" or btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O" or btn1["text"] == "O" and btn5["text"] == "O" and btn9["text"] == "O" or btn3["text"] == "O" and btn5["text"] == "O" and btn7["text"] == "O" or btn1["text"] == "O" and btn4["text"] == "O" and btn7["text"] == "O" or btn2["text"] == "O" and btn5["text"] == "O" and btn8["text"] == "O" or btn3["text"] == "O" and btn6["text"] == "O" and btn9["text"] == "O"):
         winner = True
         messagebox.showinfo("TicTacToe", "SpeletajsO ir uzvarējis!")
+        disableButtons()
     elif count == 9 and winner == False:
         messagebox.showinfo("TicTacToe", "Neizšķirts")
-
-def disableButtons():
-    btn1.config(status = DISABLED)
-    btn2.config(status = DISABLED)
-    btn3.config(status = DISABLED)
-    btn4.config(status = DISABLED)
-    btn5.config(status = DISABLED)
-    btn6.config(status = DISABLED)
-    btn7.config(status = DISABLED)
-    btn8.config(status = DISABLED)
-    btn9.config(status = DISABLED)
-    return 0
-
-def reset():
-    btn1.config(status = NORMAL)
-    btn2.config(status = NORMAL)
-    btn3.config(status = NORMAL)
-    btn4.config(status = NORMAL)
-    btn5.config(status = NORMAL)
-    btn6.config(status = NORMAL)
-    btn7.config(status = NORMAL)
-    btn8.config(status = NORMAL)
-    btn9.config(status = NORMAL)
-    btn1["text"]=" "
-    btn2["text"]=" "
-    btn3["text"]=" "
-    btn4["text"]=" "
-    btn5["text"]=" "
-    btn6["text"]=" "
-    btn7["text"]=" "
-    btn8["text"]=" "
-    btn9["text"]=" "
-    return 0
-
-def restart():
-    global winner, count, speletajsX
-    winner=False
-    count=0
-    speletajsX=True
-    return 0
+        disableButtons()
 
 
 
 
 
-def infoLogs():
-    jaunsLogs=Toplevel()
-    jaunsLogs.title('Info par programmu')
-    jaunsLogs.geometry("300x300")
-    apraksts=Label(jaunsLogs,text='Spēles noteikumi')
-    apraksts.grid(row=0,column=0)
-    return 0
+
+
 
 
 
